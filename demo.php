ad4d3b03ee5d7566e6db23597b11932a520fd417
Pushti102 <77767439+Pushti102@users.noreply.github.com>
2021-01-21 14:45:20 +0530

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,6 @@
+<?php  
+$name=$_POST["name"];//receiving name field value in $name variable  
+$email=$_POST["email"];//receiving email field value in $password variable  
+  
+echo "Transaction successfully to: $name, email: $email";  
+?>  
